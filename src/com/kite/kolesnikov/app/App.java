4d2cd8e4cb5a480a6a7a4d74126a0dd797ba7657
Alexander Kolesnikov <kolesnikov.kite@gmail.com>
2023-06-03 19:34:24 +0100

    forced push

--- src/com/kite/kolesnikov/app/App.java
+++ src/com/kite/kolesnikov/app/App.java
@@ -3,6 +3,6 @@ package com.kite.kolesnikov.app;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Магазин откроется через 2 месяца");
+        System.out.println("1234");
     }
 }
