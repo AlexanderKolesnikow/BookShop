538989142802ceedc95dd201474ec7cbd78e2377
Alexander Kolesnikov <kolesnikov.kite@gmail.com>
2023-06-03 19:38:53 +0100

    fixing conflict

--- src/com/kite/kolesnikov/app/App.java
+++ src/com/kite/kolesnikov/app/App.java
@@ -3,6 +3,6 @@ package com.kite.kolesnikov.app;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("1234");
+        System.out.println("12345");
     }
 }
